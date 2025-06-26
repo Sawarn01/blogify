199d3f9b785a4bd57967e16b0e48f0a2874588a8
Vivek Kumar <vivek@raktacure.ai>
2025-06-26 08:55:31 +0000

    [Violation] Potential permissions policy violation: geolocation is not a

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,21 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Permissions-Policy',
+            // Disable geolocation access.
+            value: 'geolocation=()',
+          },
+        ],
+      },
+    ];
+  },
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
